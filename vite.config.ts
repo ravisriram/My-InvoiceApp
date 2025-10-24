265311136499143b4d22429dc225dce157178e5d
ravisriram <ravikumar.sriram@gmail.com>
2025-10-24 12:40:47 +0530

    package jason fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+    },
+  },
   optimizeDeps: {
+    esbuildOptions: {
+      plugins: [],
+    },
     exclude: ['lucide-react'],
   },
+  base: '/My-InvoiceApp/', // GitHub Pages path
 });
