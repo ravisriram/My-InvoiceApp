dcfb8d0e656d74f9fb4ce4c200126029e6f306ce
ravisriram <ravikumar.sriram@gmail.com>
2025-10-24 12:09:54 +0530

    vite config fix v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base:"/My-InvoiceApp",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-    },
-  },
-});
+  plugins: [react()], 
+  base:"/My-InvoiceApp"
+})
